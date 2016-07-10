b3405cc933272678d2dfc45a4501c9e2a0ba3684
Denis <derk_sin@mail.ru>
2016-07-10 17:58:34 +0500

    Доработки#2
    Минифицированный css.
    Добавлены новые плагины для gulp.
    Добавлен README.md

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,50 @@ var gulp = require('gulp'), // Подключаем плагин gulp
 	concat = require('gulp-concat'), // Подключаем плагин gulp-concat, чтобы на выходе получался один файл стилей
 	plumber = require('gulp-plumber'),
 	errorHandler = require('gulp-plumber-error-handler'),
-	notify = require('gulp-notify');
+	notify = require('gulp-notify'),
+	cssnano = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+    rename	= require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+	sourcemaps = require('gulp-sourcemaps'), // Подключаем sourcemap
+	autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 // Создаем задачу для Stylus
 gulp.task('compile_stylus', function () {
 
-   gulp.src(['src/styles/style.styl']) // Подаем на вход файлы с расширением .styl из папки src/styles/blocks/ 
+   return gulp.src(['src/styles/style.styl']) // Подаем на вход файл style.styl
+   
+	    .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")})) // Выводит окно ошибки
 
-   		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")})) // Выводит окно ошибки
-
-  
-        .pipe(stylus()) // Запускаем обработчик Stylus, в потоке — style.css    
-	
-		.pipe(concat('style.css')) // Склеиваем все stylus файлы в style.css
+	    .pipe(sourcemaps.init())
+   
+	        .pipe(stylus()) // Запускаем обработчик Stylus, в потоке — style.css    
 		
+			.pipe(concat('style.css')) // Склеиваем все stylus файлы в style.css
+
+			.pipe(cssnano()) // Сжимаем
+
+			.pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+	   
+	   		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) 
+
+   		.pipe(sourcemaps.write('./'))
+
         .pipe(gulp.dest('dist/styles/')); // Копируем файл style.css в папку dist/styles/    
 });
+
+// // Создаем задачу для минификации css
+// gulp.task('css-min', ['compile_stylus'], function() {
+//     gulp.src('dist/styles/style.css') // Выбираем файл для минификации
+	
+// 	.pipe(sourcemaps.init())
+	
+//         .pipe(cssnano()) // Сжимаем
+	
+//         .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+	
+// 	.pipe(sourcemaps.write('./'))
+	
+//         .pipe(gulp.dest('dist/styles/')); // Выгружаем в папку dist/styles/
+// });
 
 // Создаем задачу watch, которая следит за измненеиями файлов с расширением .styl
 gulp.task('watch', function () {
